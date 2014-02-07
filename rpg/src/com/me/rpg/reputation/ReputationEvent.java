2b206c592e890c5305e86dddb730d9610697893e
David Mansen <davidmansen@gmail.com>
2014-02-07 13:53:34 -0500

    Just committing some unfinished stuff to work at home.

---
+++ rpg/src/com/me/rpg/reputation/ReputationEvent.java
@@ -0,0 +1,18 @@
+package com.me.rpg.reputation;
+
+import com.me.rpg.GameCharacter;
+
+public class ReputationEvent {
+	private EventID id;
+	//private coordinate coor;
+	//private Time time;
+	private int repImpactMagnitude;
+	private EventTemplate eventTemplate;
+	private int referenceCount;
+	
+	public ReputationEvent(String eventType, String groupAffected, GameCharacter characterAffected,
+			)
+	{
+		
+	}
+}
