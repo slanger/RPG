a0a7fc17c1c1cf5e89d90422a371b2efeaa6da86
David Mansen <davidmansen@gmail.com>
2014-02-11 13:14:45 -0500

    Added more class structure

--- rpg/src/com/me/rpg/reputation/EventTemplate.java
+++ rpg/src/com/me/rpg/reputation/EventTemplate.java
@@ -1,11 +1,32 @@
 package com.me.rpg.reputation;
 
 public class EventTemplate {
-	//eventName:  Attacked, Killed, StoleFrom, CompletedQuestFor
-	//repEvents
 	
+	private String eventName; 
+	private int magnitude;
+	//private String repEffects;
 	
+	EventTemplate(String eventName, int magnitude)
+	{
+		this.setEventName(eventName);
+		this.setMagnitude(magnitude);
+	}
+
+	public String getEventName() {
+		return eventName;
+	}
+
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+
+	public int getMagnitude() {
+		return magnitude;
+	}
+
+	public void setMagnitude(int magnitude) {
+		this.magnitude = magnitude;
+	}
 	
-	String eventName; 
-	String repEffects;
+
 }
